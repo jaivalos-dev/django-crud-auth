da4e0619786084ce9ba76b4060fbd0f267847140
jaivalos-dev <jaivalos.dev@gmail.com>
2024-02-23 16:37:12 -0600

    Login, logout and signup created

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 
@@ -19,7 +19,8 @@ def signup(request):
         if request.POST['password1'] == request.POST['password2']:
             try:
                 # Registrar usuario
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
                 user.save()
                 login(request, user)
                 return redirect('tasks')
@@ -33,10 +34,31 @@ def signup(request):
             'error': 'Password do not match'
         })
 
+
 def signout(request):
     logout(request)
     return redirect('home')
 
+
 def tasks(request):
     return render(request, 'tasks.html')
 
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'User or pass incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
